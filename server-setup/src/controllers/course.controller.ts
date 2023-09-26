03bfc6d3880988935ca5cf064cb22751e5f632a0
jumma <aarifhingorja4@gmail.com>
2023-09-26 16:56:26 +0530

    added some course model  apis

--- server-setup/src/controllers/course.controller.ts
+++ server-setup/src/controllers/course.controller.ts
@@ -1,72 +1,309 @@
 require("dotenv").config();
 import { Request, Response, NextFunction } from "express";
 import cloudinary from "cloudinary";
-import courseModal, {ICourse} from "../models/course.model";
+import courseModal, { ICourse } from "../models/course.model";
 import ErrorHandler from "../utils/ErrorHandler";
 import { catchAsyncError } from "../middleware/catchAsycnErrors";
 import { redis } from "../utils/redis";
 import { IGetUserAuthInfoRequest } from "../middleware/auth";
 import { createCourse } from "../services/course.service";
+import mongoose from "mongoose";
+import path from "path";
+import ejs from "ejs";
+import sendMail from "../utils/sendMail";
 
 // upload course
 export const uploadCourse = catchAsyncError(
-    async (req: Request, res: Response, next: NextFunction) => {
-        try {
-            const data = req.body;
-            const thumbnail = data.thumbnail;
+  async (req: Request, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
 
-            if(thumbnail) {
-                const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
-                    folder: "courses",
-                });
+      if (thumbnail) {
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
 
-                data.thumbnail = {
-                    public_id: myCloud.public_id,
-                    url: myCloud.secure_url,
-                }
-            }
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
 
-            await createCourse(data, res,next);
-
-        } catch (error) {
-            return next(new ErrorHandler(error.message, 500));
-        }
+      await createCourse(data, res, next);
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
     }
+  }
 );
-
 
 // edit course
 
 export const editCourse = catchAsyncError(
-    async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
-        try {
-             const data = req.body;
-             const thumbnail = data.thumbnail;
-             if(thumbnail) {
-                await cloudinary.v2.uploader.destroy(thumbnail.public_id);
-                 const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
-                     folder: "courses",
-                 });
+  async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+    try {
+      const data = req.body;
+      const thumbnail = data.thumbnail;
+      if (thumbnail) {
+        await cloudinary.v2.uploader.destroy(thumbnail.public_id);
+        const myCloud = await cloudinary.v2.uploader.upload(thumbnail, {
+          folder: "courses",
+        });
 
-                 data.thumbnail = {
-                     public_id: myCloud.public_id,
-                     url: myCloud.secure_url,
-                 }
-             }
+        data.thumbnail = {
+          public_id: myCloud.public_id,
+          url: myCloud.secure_url,
+        };
+      }
 
-             const courseId = req.params.id;
-             const course = await courseModal.findByIdAndUpdate(courseId, {
-                $set: data,
-                new: true
-             });
+      const courseId = req.params.id;
+      const course = await courseModal.findByIdAndUpdate(courseId, {
+        $set: data,
+        new: true,
+      });
 
-             res.status(201).json({
-                success: true,
-                course
-             })
-
-        } catch (error) {
-            return next(new ErrorHandler(error.message, 500));
-        }
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
     }
-)
+  }
+);
+
+// get single course -- without purchasing
+
+export const getSingleCourse = catchAsyncError(
+  async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+    try {
+      const courseId = req.params.id;
+
+      const isCachedExist = await redis.get(courseId);
+
+      //   console.log("Hitting redis")
+      if (isCachedExist) {
+        const course = JSON.parse(isCachedExist);
+
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      } else {
+        const course = await courseModal
+          .findById(req.params.id)
+          .select(
+            "-courseData.videoUrl -courseData.suggestion -courseData.question -courseData.links"
+          );
+
+        //   console.log("Hitting Mongodb")
+
+        await redis.set(courseId, JSON.stringify(course));
+
+        res.status(201).json({
+          success: true,
+          course,
+        });
+      }
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// get all course
+export const getAllCourse = catchAsyncError(
+  async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+    try {
+      const isCachedExist = await redis.get("allCourses");
+      console.log("Hitting redis");
+      if (isCachedExist) {
+        const course = JSON.parse(isCachedExist);
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      } else {
+        const course = await courseModal
+          .find()
+          .select(
+            "-courseData.videoUrl -courseData.suggestion -courseData.question -courseData.links"
+          );
+
+        console.log("Hitting Mongodb");
+        await redis.set("allCourses", JSON.stringify(course));
+
+        res.status(200).json({
+          success: true,
+          course,
+        });
+      }
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// get course content ---only for valid users
+
+export const getCourseContent = catchAsyncError(
+  async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+    try {
+      const userCourseList = req?.user?.courses;
+      const courseId = req.params.id;
+
+      const courseExist = userCourseList?.find(
+        (course: any) => course._id === courseId
+      );
+
+      if (!courseExist) {
+        return next(
+          new ErrorHandler("Your are not eligible to access this course", 404)
+        );
+      }
+
+      const course = await courseModal.findById(courseId);
+
+      const content = await course?.courseData;
+
+      res.status(200).json({
+        success: true,
+        content,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// add question in course
+interface IAddQuestionData {
+  question: string;
+  courseId: string;
+  contentId: string;
+}
+
+export const addQuestion = catchAsyncError(
+  async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+    try {
+      const { question, courseId, contentId }: IAddQuestionData = req.body;
+      const course = await courseModal.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 404));
+      }
+
+      const courseContent = course?.courseData?.find((item: any) =>
+        item._id.equals(contentId)
+      );
+
+      if (!courseContent) {
+        return next(new ErrorHandler("Invalid content id", 404));
+      }
+
+      //   create a new questions object
+      const newQuestions: any = {
+        user: req.user,
+        question,
+        questionReplies: [],
+      };
+
+      // add this question to our course content
+      courseContent.questions.push(newQuestions);
+
+      // save the update course
+
+      await course.save();
+
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
+
+// add answer in course
+
+interface IAddAnswerData {
+  answer: string;
+  courseId: string;
+  contentId: string;
+  questionId: string;
+}
+
+export const addAnswer = catchAsyncError(
+  async (req: IGetUserAuthInfoRequest, res: Response, next: NextFunction) => {
+    try {
+      const { answer, courseId, contentId, questionId }: IAddAnswerData =
+        req.body;
+
+      const course = await courseModal.findById(courseId);
+
+      if (!mongoose.Types.ObjectId.isValid(contentId)) {
+        return next(new ErrorHandler("Invalid content id", 404));
+      }
+
+      const courseContent = course?.courseData?.find((item: any) =>
+        item._id.equals(contentId)
+      );
+
+      if (!courseContent) {
+        return next(new ErrorHandler("Invalid content id", 404));
+      }
+
+      const question = courseContent?.questions?.find((item: any) =>
+        item._id.equals(questionId)
+      );
+
+      if (!question) {
+        return next(new ErrorHandler("Invalid question id", 404));
+      }
+
+      //   create new answer object
+      const newAnswer: any = {
+        user: req.user,
+        answer,
+      };
+
+      // add this answer to course course content
+      question.questionReplies.push(newAnswer);
+
+      // save the update course
+      await course.save();
+
+      if (req.user?._id === question.user?._id) {
+        // create notification
+      } else {
+        const data = {
+          name: question.user.name,
+          title: courseContent.title,
+        };
+
+        const html = await ejs.renderFile(
+          path.join(__dirname, "../mails/question-replay.ejs"),
+          data
+        );
+
+        try {
+          await sendMail({
+            email: question.user.email,
+            subject: "Question replay",
+            template: "question-replay.ejs",
+            data,
+          });
+        } catch (error: any) {
+          return next(new ErrorHandler(error.message, 500));
+        }
+      }
+      res.status(201).json({
+        success: true,
+        course,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  }
+);
--- server-setup/src/routes/course.route.ts
+++ server-setup/src/routes/course.route.ts
@@ -1,5 +1,5 @@
 import { authorizeRoles, isAuthenticated } from "../middleware/auth";
-import { editCourse, uploadCourse } from "../controllers/course.controller";
+import { addAnswer, addQuestion, editCourse, getAllCourse, getCourseContent, getSingleCourse, uploadCourse } from "../controllers/course.controller";
 import express from "express";
 
 
@@ -8,5 +8,13 @@ const courseRouter = express.Router();
 
 courseRouter.post("/create-course",isAuthenticated,authorizeRoles("admin"), uploadCourse);
 courseRouter.patch("/edit-course/:id",isAuthenticated,authorizeRoles("admin"), editCourse);
+courseRouter.get("/get-course/:id", getSingleCourse);
+courseRouter.get("/get-courses", getAllCourse);
+
+courseRouter.get("/get-course-content/:id", isAuthenticated, getCourseContent);
+courseRouter.patch("/add-question", isAuthenticated, addQuestion);
+courseRouter.patch("/add-answer", isAuthenticated, addAnswer);
+
+
 
 export default courseRouter;
